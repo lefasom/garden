1487de71a7bb2f8175bcfafccd0c1dba56601930
lefasom <leosombra89@gmail.com>
2025-01-27 22:24:17 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './header.css'
+function Header() {
+  return (
+    <div>
+      <nav></nav>
+      <div className='presentation'>
+        <img src="../../public/garden.jpg" alt="" />
+        <article>
+          <h2>Design Your Home, Your Way</h2>
+          <h6>Habitant morbi tristique senectus et. Nec dui nunc mattis enim ut tellus. Semper auctor neque vitae tempus quam pellentesque nec nam aliquam. </h6>
+          <p>Image from </p>
+          <button> view more</button>
+        </article>
+      </div>
+    </div>
+  )
+}
+
+export default Header
