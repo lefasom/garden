c6202a0d59b27438efc89c7dc87992983394b5c0
lefasom <leosombra89@gmail.com>
2025-01-27 22:26:51 -0300

    prub header phote

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     <div>
       <nav></nav>
       <div className='presentation'>
-        <img src="../../public/garden.jpg" alt="" />
+        <img src="https://gf-paysagiste.fr/wp-content/uploads/2024/01/jardin-ecolo.jpg" alt="" />
         <article>
           <h2>Design Your Home, Your Way</h2>
           <h6>Habitant morbi tristique senectus et. Nec dui nunc mattis enim ut tellus. Semper auctor neque vitae tempus quam pellentesque nec nam aliquam. </h6>
