f54df3c6e296625279a74b0f35121443a983c2d2
lefasom <leosombra89@gmail.com>
2025-01-28 21:35:05 -0300

    add grid

---
+++ src/components/Grid.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './grid.css'
+function Grid() {
+  return (
+    <div className='grid'>
+        <img src="https://assets.nicepagecdn.com/d2cc3eaa/88834/images/ff3.jpg" alt="" />
+        <img src="https://assets.nicepagecdn.com/d2cc3eaa/88834/images/ffg4.jpg" alt="" />
+        <img src="https://assets.nicepagecdn.com/d2cc3eaa/88834/images/dfdfe.jpg" alt="" />
+        <img src="https://assets.nicepagecdn.com/d2cc3eaa/88834/images/ddddd.jpg" alt="" />
+        <img src="https://assets.nicepagecdn.com/d2cc3eaa/88834/images/rttr.jpg" alt="" />
+        <img src="https://assets.nicepagecdn.com/d2cc3eaa/88834/images/333.jpg" alt="" />
+    </div>
+  )
+}
+
+export default Grid
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     <div>
       <nav></nav>
       <div className='presentation'>
-        <img src="https://gf-paysagiste.fr/wp-content/uploads/2024/01/jardin-ecolo.jpg" alt="" />
+        <img src="https://assets.nicepagecdn.com/d2cc3eaa/88834/images/ffg4.jpg" alt="" />
         <article>
           <h2>Design Your Home, Your Way</h2>
           <h6>Habitant morbi tristique senectus et. Nec dui nunc mattis enim ut tellus. Semper auctor neque vitae tempus quam pellentesque nec nam aliquam. </h6>
@@ -13,6 +13,12 @@ function Header() {
           <button> view more</button>
         </article>
       </div>
+      <section>
+        <h3>Build Gre​at Products</h3>
+        <p>
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.
+        </p>
+      </section>
     </div>
   )
 }
