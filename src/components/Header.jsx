b12f84e6372a533f4b24923c1a51e69b2a6359c7
lefasom <leosombra89@gmail.com>
2025-02-09 21:15:09 -0300

    add icon wsp and ig

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,41 @@
-import React from 'react'
-import './header.css'
+import React, { useState } from 'react';
+import './header.css';
+
 function Header() {
+  const [view, setView] = useState(false);
+
+  const handleContactClick = () => {
+    setView(!view);
+  };
+
   return (
     <div>
       <nav>
         <div style={{ backgroundImage: "url('./servicio.png')" }}></div>
-
       </nav>
-      <div className='presentation'>
+      <div className="presentation">
         <div className="img" style={{ backgroundImage: "url('./fondo1.png')" }}></div>
         <article>
           <h2>Transforma tu Jardín</h2>
           <h6>Expertos en diseño y cuidado de espacios verdes. Relájate y deja el trabajo en nuestras manos.</h6>
           <p>¡Nosotros lo hacemos por ti!</p>
-          <button>Descubre más</button>
+          <button onClick={handleContactClick}>Contáctanos</button>
         </article>
-
       </div>
-      <div className='section-div'>
-        <section>
-
-        </section>
-        <section>
-
-        </section>
-        <section>
-
-        </section>
+      <div className="section-div">
+        <section></section>
+        <section></section>
+        <section></section>
       </div>
 
+      {view !== false && (
+        <div className="random-number">
+          <div className="icon" style={{ backgroundImage: "url('./social.png')" }}></div>
+          <div className="icon" style={{ backgroundImage: "url('./whatsapp.png')" }}></div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
