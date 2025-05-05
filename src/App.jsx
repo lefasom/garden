6b1673b8f03e6b17734ac990f4b0135ce5dbddc3
lefasom <leosombra89@gmail.com>
2025-05-04 21:01:15 -0300

    ajuste Gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import GardeningServicesGrid from './components/GardeningServicesGrid'
-import Footer from './components/Footer'
-import ServiciosJardineria from './components/ServiciosJardineria'
-import ScrollToTopButton from './components/ScrollToTopButton'
-import Gallery from './components/gallery'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import GardeningServicesGrid from "./components/GardeningServicesGrid";
+import Footer from "./components/Footer";
+import ServiciosJardineria from "./components/ServiciosJardineria";
+import ScrollToTopButton from "./components/ScrollToTopButton";
+import Gallery from "./components/Gallery";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <ScrollToTopButton/>
-    <Header/>
-    <ServiciosJardineria/>
-    <GardeningServicesGrid/>
-    <Gallery/>
-    <Footer/>
+      <ScrollToTopButton />
+      <Header />
+      <ServiciosJardineria />
+      <GardeningServicesGrid />
+      <Gallery />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
